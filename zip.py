7eb73b03037b361a241a54605c5ffaf4b3d25e82
Вадим Бажов <master@remort.net>
2021-01-09 15:33:20 +0300

    Two more zip() memos.

--- zip.py
+++ zip.py
@@ -9,3 +9,10 @@ for index, item in enumerate(a):
 # change mutable iterable elements while walk over them with zip()
 for x, y in zip(a,b):
     x['age'] = y
+
+# unzip of flat iterators. use .pop() instead next() for list
+a = (x for x in range(105))
+list((tuple(next(a) for x in range(10)) for y in range(10))) + [x for x in (tuple(a),) if x]
+
+# 10x10 matrix has been flatten by zip()
+list(zip(*([x for x in range(100)][i::10] for i in range(10))))
